62bf843b69b65031d7b19c44eb1a7d62f2651ead
Brian Buchalter <bal711@gmail.com>
2013-12-05 21:08:13 -0700

    Reduce method name length

--- test/board_test.rb
+++ test/board_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 require 'board'
 
 class BoardTest < Minitest::Test
-  def test_initial_state_has_no_players
+  def test_new_board_has_no_players
     board = new_board
     assert_new_board_state(board)
   end
